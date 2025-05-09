a74e78b9b700ef5e8ca01e5f30b63a895731d589
Matt2e2 <43849068+Matt2e2@users.noreply.github.com>
2025-05-10 02:27:24 +0300

    remove what

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { motion, useDragControls } from 'motion/react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { i } from 'motion/react-client'
 
 type Piece = '♔' | '♕' | '♖' | '♗' | '♘' | '♙' | '♚' | '♛' | '♜' | '♝' | '♞' | '♟' | ' '
 const whitePieces = ['♔' , '♕' , '♖' , '♗' , '♘' , '♙']
